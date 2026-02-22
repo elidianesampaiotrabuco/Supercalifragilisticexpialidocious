cf737f585941c8b0bc1673bd2e77dc9a0268e0eb
Elidiane <121459020+elidianesampaiotrabuco@users.noreply.github.com>
2026-02-22 00:24:51 -0300

    sadly a gdps is in the mod now!? + replace a bunch of links

--- src/links.cpp
+++ src/links.cpp
@@ -2,16 +2,16 @@
 
 using namespace geode::prelude;
 
-inline static std::string server = Mod::get()->getDescription().value_or("remina.bccst.ru");
+inline static std::string server = Mod::get()->getDescription().value_or("sadlya.ps.fhgdps.com");
 inline static auto links = matjson::parse(R"({
 	"asdasd": "asdasd",
 	"https://www.boomlings.com/GDEditor": "https://github.com/lil2kki/REMINA/wiki/Editor-Guide-(New-Features-List)#",
-	"https://www.robtopgames.com": "https://t.me/lil2kki_ch/6",
-	"https://www.boomlings.com/database/accounts/accountManagement.php": "https://remina.bccst.ru/dashboard/",
-	"https://store.steampowered.com/recommended/recommendgame/322170": "https://gdpshub.com/gdps/900",
+	"https://www.robtopgames.com": "https://elidianesampaiotrabuco.github.io",
+	"https://www.boomlings.com/database/accounts/accountManagement.php": "https://sadlya.ps.fhgdps.com",
+	"https://store.steampowered.com/recommended/recommendgame/322170": "https://gdpshub.com/gdps/5556",
 	"https://discord.com/invite/geometrydash": "https://discord.gg/NXbbv2HZGg",
-	"https://twitter.com/robtopgames": "https://t.me/lil2kki_ch",
-	"https://www.youtube.com/user/RobTopGames": "https://www.youtube.com/channel/UCpgyf8EpL62WyDM2jWo4v1w"
+	"https://twitter.com/robtopgames": "https://x.com/MSVE640",
+	"https://www.youtube.com/user/RobTopGames": "https://peertube.wtf/c/msve640/videos"
 })").unwrapOrDefault();
 
 //send
@@ -31,7 +31,7 @@ class $modify(CCApplicationLinksReplace, CCApplication) {
 	$override void openURL(const char* psz) {
 		std::string url = psz;
 		url = not links.contains(url) ? url : links[url].asString().unwrapOr(url);
-		url = string::replace(url, "https://www.twitter.com/", "https://t.me/");
+		url = string::replace(url, "https://www.twitter.com/", "https://x.com/");
 		url = string::replace(url, "www.boomlings.com/database", server);
 		url = string::replace(url, "boomlings.com/database", server);
 		//log::debug("{}.url = {}", __FUNCTION__, url);
